0f8c82da5b17262185566448863fb0afa749623e
Andrea Gallidabino <andrea.gallidabino@usi.ch>
2020-12-30 18:49:42 +0100

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -92,20 +92,25 @@ module.exports = {
     },
   },
 
-  externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'React',
-      root: 'React',
+  externals: [
+    {
+      react: {
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'React',
+        root: 'React',
+      },
+      'react-dom': {
+        commonjs: 'react-dom',
+        commonjs2: 'react-dom',
+        amd: 'ReactDOM',
+        root: 'ReactDOM',
+      },
     },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'ReactDOM',
-      root: 'ReactDOM',
-    },
-  },
+    /@material-ui\/core\/.*/,
+    /@material-ui\/icons\/.*/,
+    /@material-ui\/lab\/.*/,
+  ],
 
   optimization: {
     usedExports: true,
